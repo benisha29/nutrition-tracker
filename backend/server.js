5b5f87430aa743df078a0111aba3359515a02883
ben <benishasharma29@gmail.com>
2026-02-27 20:16:32 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Nutrition Tracker API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
